19b0b06d362320064af4637c3dd38b33886e5dfd
Robert Czikkel <robertczikkel@Roberts-MacBook-Pro.local>
2021-07-27 10:44:00 +0100

    error corrected

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchComments, setPermaLink, setId } from '../features/comments/commentSlice';
 import Comments from '../features/comments/comments';
 import { setVisible, upVote, downVote } from '../features/redditHome/redditSlice'
